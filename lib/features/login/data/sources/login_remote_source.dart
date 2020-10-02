69b8c911830aad87297f9e1fb63190370008572a
shivalita <shivalita@pm.me>
2020-10-02 14:24:59 +0200

    add features

--- lib/features/login/data/sources/login_remote_source.dart
+++ lib/features/login/data/sources/login_remote_source.dart
@@ -1,1 +1,24 @@
 //! Connexion au compte Google
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class LoginRemoteSource {
+
+  String api = "http://94.23.11.60:8081/memoservices/api/v2/";
+
+  FindUser() async {
+    var link = api+'user/find';
+    return await http.post(
+      link,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        "email": "00708valentin@gmail.com",
+        "user": "799",
+      }),
+    );
+  }
+
+}
