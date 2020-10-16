74bcdf2108a27b82d66d891639baf61e7274e9d9
Antonin06 <avon.antonin@gmail.com>
2020-10-16 16:49:59 +0200

    masonry_grid

--- lib/features/souvenirs/data/sources/list_souvenirs_remote_source.dart
+++ lib/features/souvenirs/data/sources/list_souvenirs_remote_source.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:memolidays/core/components/exceptions/api_exception.dart';
 import 'package:memolidays/features/souvenirs/domain/models/category.dart';
--- lib/features/souvenirs/view/components/category_component.dart
+++ lib/features/souvenirs/view/components/category_component.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:memolidays/features/souvenirs/dependencies.dart';
 import 'package:memolidays/features/souvenirs/domain/models/category.dart';
 
+// ignore: must_be_immutable
 class CategoryComponent extends StatelessWidget {
 
   String chargement = "waiting"; //!
@@ -10,30 +11,25 @@ class CategoryComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: souvenirsState.whenRebuilder(
-      initState: () => souvenirsState.setState((state) async => await state.getCategoriesList(context)),
-      onIdle: () => CircularProgressIndicator(),
-      onWaiting: () => CircularProgressIndicator(),
-      onError: (error) => Text(error.toString()),
-      onData: () {
-        return Container(
         // return chargement == "idle" ? Container( //!
-        child: Column(children: <Widget>[
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {Get.toNamed('/');},
-                  child: rowChips(),
-                )
-              ],
+          child: Column(
+            children: <Widget>[
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {Get.toNamed('/');},
+                    child: rowChips(),
+                  )
+                ],
+              ),
             ),
-          ),
-      ]));
+            ]
+          )
+        );
       // ])) : //!
       // chargement == "waiting" ?  //!
-    }));
   }
 
   Widget rowChips() {
@@ -46,14 +42,14 @@ class CategoryComponent extends StatelessWidget {
     });
 
     return Row(
-      children: widgetsList,
-      // children: <Widget>[
-      //   chipForRow('Health', Color(0xFFff8a65)),
-      //   chipForRow('Food', Color(0xFF4fc3f7)),
-      //   chipForRow('Lifestyle', Color(0xFF9575cd)),
-      //   chipForRow('Sports', Color(0xFF4db6ac)),
-      //   chipForRow('Nature', Color(0xFF5cda65)),
-      // ],
+      // children: widgetsList,
+      children: <Widget>[
+        chipForRowDisable('Disable', Color(0xFFd3d3d3)),
+        chipForRowDisable('Disable', Color(0xFFd3d3d3)),
+        chipForRow('Selected', Color(0xFFFF9800)),
+        chipForRowDisable('Disable', Color(0xFFd3d3d3)),
+        chipForRowDisable('Disable', Color(0xFFd3d3d3)),
+      ],
     );
   }
 
@@ -65,12 +61,33 @@ class CategoryComponent extends StatelessWidget {
         label: Text(
           label,
           style: TextStyle(
-            color: Colors.white,
+            color: Colors.black,
+            fontWeight: FontWeight.w600
           ),
         ),
         
         backgroundColor: color,
         elevation: 6.0,
+        shadowColor: Colors.grey[60],
+        padding: EdgeInsets.all(6.0),
+      ),
+    );
+  }
+
+   Widget chipForRowDisable(String label, Color color) {
+    return Container(
+      margin: EdgeInsets.all(6.0),
+      child: Chip(
+        labelPadding: EdgeInsets.all(5.0),
+        label: Text(
+          label,
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        
+        backgroundColor: color,
+        elevation: 3.0,
         shadowColor: Colors.grey[60],
         padding: EdgeInsets.all(6.0),
       ),
